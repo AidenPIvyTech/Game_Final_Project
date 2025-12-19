5e57125eef937b5dc19202726b3781cc98868774
Landenivy-tech <10002646@stu.nwcs.k12.in.us>
2025-12-19 09:03:20 -0500

    Add files via upload

---
+++ new-game-project/static and rigid/wall.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+var levels_Beat = 0;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+#if getposition == (0, 0)
+# turn on absorbment inthe rigid body it slows down on bounces
+#add a counter/++ if they beat a level and one and then go to the next level 
